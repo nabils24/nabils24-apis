7f5eadf00b21ce57b1c94a5c4842f1a10912bbbc
Nabil Sahsada Suratno <nabilsahsadacode@gmail.com>
2022-07-16 16:31:51 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b2482d1cfb185f741549b54688cd5ef0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8900f7b9-49ac-4444-afde-d1936aa64bc6'
+};
